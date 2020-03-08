2a58f37ff33839d2514dd056ea6d90bfb0d02a71
John Yoder <john.yoder@appfolio.com>
2020-03-08 10:11:24 -0700

    Add unit tests for Error

--- test/ErrorSpec.hs
+++ test/ErrorSpec.hs
@@ -14,3 +14,9 @@ spec = do
     it "prepends the given text to the error message" $ do
       let error = Error.make "error"
       Error.prepend "some: " error `shouldBe` Error.make "some: error"
+  describe "Show" $ do
+    it "converts the error to text" $ do
+      show (Error.make "error") `shouldBe` ("T (Message \"error\")" :: Text)
+  describe "Eq" $ do
+    it "tests whether two errors are equal" $ do
+      Error.make "error1" `shouldNotBe` Error.make "error2"
