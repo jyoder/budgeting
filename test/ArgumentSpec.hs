6377dc5fd10d6da95466aeea7c37df9b3a7ab711
John Yoder <john.yoder@appfolio.com>
2020-03-07 19:50:19 -0800

    Add unit tests for Argument

---
+++ test/ArgumentSpec.hs
@@ -0,0 +1,16 @@
+module ArgumentSpec (spec) where
+
+import qualified Argument
+import Protolude
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "fromText" $ do
+    it "creates a new argument from text" $ do
+      let argument = Argument.fromText "argument"
+      Argument.toText argument `shouldBe` "argument"
+  describe "toText" $ do
+    it "converts an argument to text" $ do
+      let argument = Argument.fromText "argument"
+      Argument.toText argument `shouldBe` "argument"
