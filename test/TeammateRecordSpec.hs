222465fbf6215439aaf90ef071e8a9db130aaf9f
John Yoder <john.yoder@appfolio.com>
2020-03-08 10:12:43 -0700

    Add unit tests for TeammateRecord

--- test/TeammateRecordSpec.hs
+++ test/TeammateRecordSpec.hs
@@ -8,10 +8,19 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
+  describe "accessors" $ do
+    it "provides access to fields in the record" $ do
+      let record = TeammateRecord.T "123" "Bob Bobberson" "49ers" "Raiders" "Dolphins" "49ers"
+      TeammateRecord.bhc record `shouldBe` "123"
+      TeammateRecord.name record `shouldBe` "Bob Bobberson"
+      TeammateRecord.teamsQ1 record `shouldBe` "49ers"
+      TeammateRecord.teamsQ2 record `shouldBe` "Raiders"
+      TeammateRecord.teamsQ3 record `shouldBe` "Dolphins"
+      TeammateRecord.teamsQ4 record `shouldBe` "49ers"
   describe "decodeByName" $ do
     it "returns a TeammateRecord when all columns are present" $ do
-      decodeByName "Bhc,Name,Teams Q1,Teams Q2,Teams Q3,Teams Q4\nBob Bobberson,123,49ers,Raiders,Dolphins,49ers"
+      decodeByName "Bhc,Name,Teams Q1,Teams Q2,Teams Q3,Teams Q4\n123,Bob Bobberson,49ers,Raiders,Dolphins,49ers"
         `shouldBe` Right
           ( fromList ["Bhc", "Name", "Teams Q1", "Teams Q2", "Teams Q3", "Teams Q4"],
-            fromList [TeammateRecord.T "Bob Bobberson" "123" "49ers" "Raiders" "Dolphins" "49ers"]
+            fromList [TeammateRecord.T "123" "Bob Bobberson" "49ers" "Raiders" "Dolphins" "49ers"]
           )
